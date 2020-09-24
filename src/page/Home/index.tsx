f7ab6950269a89f2d66810b58b99206d3af93580
Thieres Lima <thieresllima@gmail.com>
2020-09-24 09:25:27 -0300

    Main page

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Container, Header, WhatsappLogo, WhatsappText } from './styles';
-import { BsChatDots } from 'react-icons/bs';
+import * as Styles from './styles';
+import { BsChatDots, BsThreeDotsVertical, } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import qrcode from '../../assets/images/qrcode.png';
+
 
 export default function Home() {
     return (
-        <Container>
-            <Header>
-                <WhatsappLogo>
+        <Styles.Container>
+            <Styles.Header>
+                <Styles.WhatsappLogo>
                     <BsChatDots size={38} color="#FFF" />
-                    <WhatsappText>WhatsApp Web</WhatsappText>
-                </WhatsappLogo>
-            </Header>
-        </Container>
+                    <Styles.WhatsappText>WhatsApp Web</Styles.WhatsappText>
+                </Styles.WhatsappLogo>
+            </Styles.Header>
+
+            <Styles.ContainerMain>
+                <Styles.Main>
+                    <Styles.Title>Para usar o WhatsApp no seu Computador:</Styles.Title>
+                    <Styles.info>
+                        <Styles.Text>1.Abra o WhatsApp no seu celular</Styles.Text>
+                        <Styles.Text>2.Toque em Mais opções <BsThreeDotsVertical size={18} /> ou Ajustes <FiSettings /> e selecione WhatsApp Web</Styles.Text>
+                        <Styles.Text>3.Aponte seu celular para essa tela para capturar o código</Styles.Text>
+
+                        <Styles.TextHelp>Precisa de ajuda para começar?</Styles.TextHelp>
+                    </Styles.info>
+                </Styles.Main>
+
+                <Styles.Qrcode src={qrcode} />
+            </Styles.ContainerMain>
+        </Styles.Container>
     );
 };
--- src/page/Home/styles.ts
+++ src/page/Home/styles.ts
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    background: #abdaea;
+    height: 100vw;
+`;
 
 export const Header = styled.div`
-    background: linear-gradient( to right, rgba(0, 218, 106), rgba(61, 176, 220));
+    background: linear-gradient( rgba(0, 218, 106), rgba(61, 176, 220));
     max-width: 100%;
-    height: 230px;
+    height: 220px;
 `;
 
 export const WhatsappLogo = styled.div`
@@ -23,4 +26,51 @@ export const WhatsappText = styled.p`
     text-transform: uppercase;
     color: #FFF;
     margin-left: 10px
+`;
+
+export const ContainerMain = styled.div`
+    display: flex;
+    width: 1000px;
+    height: 896px;
+    padding: 40px;
+    background: #FFF;
+    margin: 0 auto;
+    margin-top: -120px;
+    border-radius: 5px;
+`;
+
+export const Main = styled.div`
+    padding: 22px;
+    width: 700px;
+`;
+
+export const Title = styled.p`
+    font-size: 28px;
+    color: #959ba0;
+    font-weight: 400;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;  
+    word-spacing: -2px;
+    margin-bottom: 52px;
+`;
+
+export const Text = styled.p`
+    font-size: 20px;
+    color: #959ba0;
+    line-height: 30px;
+    margin-left: 5px;
+    padding-bottom: 20px
+`;
+
+export const info = styled.div`
+    width: 500px;
+`;
+
+export const TextHelp = styled.p`
+    color: #00BEA4;
+    margin-top: 28px;
+`;
+
+export const Qrcode = styled.img`
+    width: 400px;
+    height: 400px;
 `;
